ba55dcac011fb6f0bdc4d210b771431f11eb36d4
Blair Frandeen <blairfrandeen@gmail.com>
2023-08-19 16:28:34 -0700

    add justext option

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import sys
 import requests
 from html2text import html2text
+import justext
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Extract text from a URL")
-parser.add_argument("url", help="The URL to extract text from")
 
-args = parser.parse_args()
+def main(args):
+    response = requests.get(args.url)
+    response.raise_for_status()
 
-response = requests.get(args.url)
-response.raise_for_status()
+    html = response.text
 
-html = response.text
-md = html2text(html)
+    if args.method == "justext":
+        paragraphs = justext.justext(html, justext.get_stoplist("English"))
 
-print(md)
+        for paragraph in paragraphs:
+            if not paragraph.is_boilerplate:
+                print(paragraph.text)
+
+    elif args.method == "full":
+        md = html2text(html)
+        print(md)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract text from a URL")
+    parser.add_argument("url", help="The URL to extract text from")
+    parser.add_argument(
+        "--method",
+        default="justext",
+        choices=["justext", "full"],
+        help="Which method to use. \
+                'full' uses the html2text library and will generally give the full content of the page. \
+                'justext' will attempt to remove menus, headers, and footers to just deliver the content.",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
